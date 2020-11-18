2e3ffb6712e8fbf6403fd20b26758c0bcb57b0bb
Mehmood Khalil <38806418+iamwebsurgeon@users.noreply.github.com>
2020-11-18 17:13:17 +0500

    Add files via upload

---
+++ Lec # 14/main.js
@@ -0,0 +1,20 @@
+var name = "Mazhar";
+var age = 18;
+
+// if(age == 21){
+// 	document.write("My age is " + age);
+// }
+// else{
+// 	document.write("My age is not equal to 21");
+// }
+
+
+var day = "Wednesday";
+
+
+if(day == "Wednesday"){
+	document.write("Today is " + day);
+}
+else{
+	document.write("Today is other day");
+}
