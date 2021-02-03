f441480692beff725dd8ef8e68ff014f0700a017
Mehmood Khalil <38806418+iamwebsurgeon@users.noreply.github.com>
2021-02-03 15:01:18 +0500

    Add files via upload

---
+++ Lec # 25/batch-2.php
@@ -0,0 +1,42 @@
+<?php
+/* This is a comment */
+echo "<h1>Hello World</h1>";
+
+$firstName = "Asad";
+$lastName = "Zia";
+
+echo $firstName;
+echo $lastName;
+
+$var1 = "testing";
+
+echo var_dump($var1);
+
+// Floating values
+
+$var2 = 10.54;
+
+echo var_dump($var2);
+
+$isActive = false;
+
+echo var_dump($isActive);
+
+
+$cars = array("Honda", "Toyota", "Kia", "Suzuki");
+
+print_r($cars);
+echo "<br>";
+echo strlen("The Quick brown fox jumps over the lazy dog.");
+echo "<br>";
+echo str_word_count("The Quick brown fox jumps over the lazy dog.");
+echo "<br>";
+echo strrev("The Quick brown fox jumps over the lazy dog.");
+echo "<br>";
+echo "<br>";
+
+echo strpos("Hello World!", "world");
+echo "<br>";
+
+
+?>
